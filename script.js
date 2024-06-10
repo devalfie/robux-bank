6a41f70462067a18dc637937087e9c20a1930899
devalfie <133496544+devalfie@users.noreply.github.com>
2024-06-10 01:05:39 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitPoints() {
+    var austinPoints = document.getElementById("austin-point").value;
+    var remsPoints = document.getElementById("rems-point").value;
+
+    // You can add further validation here if needed
+
+    // Example: You can log the values to the console
+    console.log("Austin Points: " + austinPoints);
+    console.log("Rems Points: " + remsPoints);
+}
